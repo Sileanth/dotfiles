4c8528d61c284832b6ce70d045a06a34bf8e6274
sileanth <lukasz.magnuszewski@gmail.com>
2024-08-24 16:36:17 +0200

    zellij config alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,16 +13,16 @@ starship init fish | source
 
 # aliases
 alias lg="lazygit"
+alias config="zellij a config"
 
 alias ll="lsd -al"
 alias ls="lsd"
 
 
-
-# BEGIN opam configuration
+ BEGIN opam configuration
 # This is useful if you're using opam as it adds:
-#   - the correct directories to the PATH
+   - the correct directories to the PATH
 #   - auto-completion for the opam binary
-# This section can be safely removed at any time if needed.
+ This section can be safely removed at any time if needed.
 test -r '/home/sileanth/.opam/opam-init/init.fish' && source '/home/sileanth/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
 # END opam configuration
