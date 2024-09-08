2b9125f9df621ed9037ba37ec11785112ec65e5f
sileanth <lukasz.magnuszewski@gmail.com>
2024-09-08 13:23:04 +0200

    bkacup

---
+++ emacs/.config/emacs/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8ccbbbf5c197f80c9dce116408a248fde1ac4fedd9b5b7883e729eba83c9c64e" default))
+ '(org-agenda-files '("~/dotfiles/emacs/.config/emacs/test.org"))
+ '(package-selected-packages
+   '(rustic lsp-ui lsp-mode company utop flycheck-ocaml merlin-eldoc merlin dune tuareg orderless vertico marginalia catppuccin-theme which-key rainbow-delimiters magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,223 @@
+
+;;performance
+
+;increace gc threshold to 100mb
+(setq gc-cons-threshold 100000000)
+
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+
+
+;;ui
+(tool-bar-mode -1)             ; Hide the outdated icons
+(scroll-bar-mode -1)           ; Hide the always-visible scrollbar
+(setq inhibit-splash-screen t) ; Remove the "Welcome to GNU Emacs" splash screen
+(setq use-file-dialog nil)      ; Ask for textual confirmation instead of GUI
+
+
+
+;; Auto-refresh buffers when files on disk change.
+(global-auto-revert-mode t)
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+
+;; Remember and restore the last cursor location of opened files
+(save-place-mode 1)
+;; remember recent files
+(recentf-mode 1)
+
+;; I store automatic customization options in a gitignored file,
+(setq custom-file "~/.config/emacs/custom.el")
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+
+;; Place backups in a separate folder.	
+(setq backup-directory-alist `(("." . "~/.saves")))
+(setq auto-save-file-name-transforms `((".*" "~/.saves/" t)))
+					; packages
+(use-package magit
+  :ensure t)
+
+
+
+
+;; Add MELPA to your list of package archives
+(add-to-list 'package-archives
+	         '("melpa" . "https://melpa.org/packages/"))
+
+
+;; packages
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode )
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.1))
+
+(use-package catppuccin-theme
+  :ensure t
+  :init (load-theme 'catppuccin :no-confirm)
+  :config
+  
+  (setq catppuccin-flavor 'frappe) ;; or 'latte, 'macchiato, or 'mocha
+  )
+
+;; vertico config
+;; Enable vertico
+(use-package vertico
+  :ensure t
+  :custom
+  ;; (vertico-scroll-margin 0) ;; Different scroll margin
+  ;; (vertico-count 20) ;; Show more candidates
+  ;; (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
+  (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+  :init
+  (vertico-mode))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; A few more useful configurations...
+(use-package emacs
+  :custom
+  ;; Support opening new minibuffers from inside existing minibuffers.
+  (enable-recursive-minibuffers t)
+  ;; Emacs 28 and newer: Hide commands in M-x which do not work in the current
+  ;; mode.  Vertico commands are hidden in normal buffers. This setting is
+  ;; useful beyond Vertico.
+  (read-extended-command-predicate #'command-completion-default-include-p)
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode))
+
+
+;; Optionally use the `orderless' completion style.
+(use-package orderless
+  :ensure t
+  :custom
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
+  ;; (orderless-component-separator #'orderless-escapable-split-on-space)
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
+
+
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+  :ensure t
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init section is always executed.
+  :init
+
+  ;; Marginalia must be activated in the :init section of use-package such that
+  ;; the mode gets enabled right away. Note that this forces loading the
+  ;; package.
+  (marginalia-mode))
+
+
+;; org mode
+
+
+  (setq org-todo-keywords
+  '((sequence "TODO" "PROG" "WAIT" "DONE" "KILL")))
+
+
+
+;; completion
+(use-package company
+  :ensure t
+  :commands company-mode
+  :init
+  (add-hook 'prog-mode-hook #'company-mode))
+
+
+
+
+;; lsp
+(use-package lsp-mode
+  :ensure t
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         ;;(XXX-mode . lsp)
+	 ;;try lsp at every prog
+	 (c-mode-hook . lsp)
+	 (c++-mode-hook . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp
+  :custom
+    (lsp-inlay-hint-enable t)
+  )
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+
+
+;;haskell
+
+
+
+;;rust
+
+(use-package rustic
+  :ensure t
+  :config
+  (setq rustic-format-on-save t)
+  :custom
+  (rustic-cargo-use-last-stored-arguments t)
+  )
+
+;;ocaml
+
+
+(use-package tuareg
+  :ensure t
+  :mode (("\\.ocamlinit\\'" . tuareg-mode)))
+
+;; Major mode for editing Dune project files
+(use-package dune
+  :ensure t)
+
+
+;; utop configuration
+(use-package utop
+  :ensure t
+  :config
+  (add-hook 'tuareg-mode-hook #'utop-minor-mode))
+
