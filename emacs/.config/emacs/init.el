ca7a37b7dbd997edbf77d15efa0ef04cfd3ee119
sileanth <lukasz.magnuszewski@gmail.com>
2024-09-19 14:22:58 +0200

    update

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -137,6 +137,7 @@
   ;; The :init section is always executed.
   :init
 
+
   ;; Marginalia must be activated in the :init section of use-package such that
   ;; the mode gets enabled right away. Note that this forces loading the
   ;; package.
